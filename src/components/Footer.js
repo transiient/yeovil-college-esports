b7db3d2ae44fa5ebefbe36190222e5ba6ee532b2
Sam Cross <crossmansam@gmail.com>
2020-04-24 13:31:58 +0100

    style: simplify header and footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 import cn from './Footer.module.scss';
 
-//!
-// Not the same as NavLink in ./Header.js
+//! Not the same as NavLink in ./Header.js
 function NavLink(props) {
     return (
         <li className={cn.navLink}>
@@ -11,25 +10,31 @@ function NavLink(props) {
                 {props.children}
             </a>
         </li>
-    )
+    );
 }
 
 export default function Footer() {
     return (
-        <footer>
-            <div className={cn.flex}>
+        <footer className={cn.footer}>
+            <div className={cn.linkWrapper}>
                 <a href="https://yeovil.ac.uk/" rel="noopener noreferrer" target="_blank"><div className={cn.ycLogo}></div></a>
-                <nav>
+                <ul>
                     <NavLink to="https://yeovil.ac.uk/">Home</NavLink>
                     <NavLink to="https://yeovil.ac.uk/our-courses">Courses</NavLink>
                     <NavLink to="https://yeovil.ac.uk/student-life">Student Life</NavLink>
                     <NavLink to="https://yeovil.ac.uk/events">Events</NavLink>
                     <NavLink to="https://yeovil.ac.uk/contact">Contact Us</NavLink>
-                </nav>
+                </ul>
                 <div className={cn.ofstedBadge}></div>
             </div>
 
-            <div className={cn.copyright}>Design and development by <a className="inlineLink" href="http://github.com/sam-cross">Sam Cross</a> - &copy; 2020</div>
+            <div className={cn.copyright}>Design and development by <a
+                className="inlineLink"
+                href="http://github.com/sam-cross"
+                target="_blank"
+                rel="noopener noreferrer">
+                Sam Cross
+                </a> - &copy; 2020</div>
         </footer>
-    )
+    );
 }
