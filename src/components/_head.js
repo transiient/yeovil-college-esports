25ec173971d7d03c721c3420ad183dbe3e0080be
Sam Cross <crossmansam@gmail.com>
2020-04-27 13:38:09 +0100

    feat: add favicon and manifest

--- src/components/_head.js
+++ src/components/_head.js
@@ -3,22 +3,37 @@ import Head from 'next/head';
 export default () => {
     return (
         <Head>
-            <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta name="theme-color" content="#1A3663" />
             <meta name="description" content="Esports at Yeovil College - a microsite experience by students." />
-            <link href="https://fonts.googleapis.com/css?family=Fugaz+One|Nunito:400,700|Roboto:400,700&display=swap"
-                rel="stylesheet" />
-            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css"
-                integrity="sha256-gvEnj2axkqIj4wbYhPjbWV7zttgpzBVEgHub9AAZQD4=" crossOrigin="anonymous" />
-            <link rel="stylesheet" type="text/css"
+            <link
+                rel="stylesheet"
+                href="https://fonts.googleapis.com/css?family=Fugaz+One|Nunito:400,700|Roboto:400,700&display=swap" />
+            <link
+                rel="stylesheet"
+                href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+            <link
+                rel="stylesheet"
+                integrity="sha256-gvEnj2axkqIj4wbYhPjbWV7zttgpzBVEgHub9AAZQD4="
+                crossOrigin="anonymous"
+                href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css" />
+            <link
+                rel="stylesheet"
+                type="text/css"
                 href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-            <link rel="stylesheet" type="text/css"
+            <link
+                rel="stylesheet"
+                type="text/css"
                 href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-            <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-            <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-            <title>Esports at Yeovil College</title>
+
+            <link rel="manifest" href="/site.webmanifest" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1a3663" />
+            <meta name="msapplication-TileColor" content="#1a3663" />
+            <meta name="theme-color" content="#1a3663" />
+
+            <title>Yeovil College - Esports</title>
         </Head>
     );
 };
